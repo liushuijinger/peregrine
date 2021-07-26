e120d2632d6f6fd77f55ff7c1af9bd86c45ec3a6
shuijing <shuijing@shanshu.ai>
2021-07-26 15:15:50 +0800

    删除多余代码

--- src/main/java/com/shuijing/peregrine/common/config/ValidatorConfiguration.java
+++ src/main/java/com/shuijing/peregrine/common/config/ValidatorConfiguration.java
@@ -2,8 +2,6 @@ package com.shuijing.peregrine.common.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -16,11 +14,6 @@ public class ValidatorConfiguration extends WebMvcConfigurationSupport {
 
     @Override
     public Validator getValidator() {
-        return validator();
-    }
-
-    @Bean
-    public Validator validator() {
         LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
         validator.setValidationMessageSource(messageSource);
         return validator;
